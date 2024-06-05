118cb2999626092e1963c907c2778a710ae24eba
Adrian Flores <adrian@sforce.tech>
2024-06-05 13:20:42 -0400

    feat: updated UI and navigation

--- src/pages/ManageUsersPage.tsx
+++ src/pages/ManageUsersPage.tsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
-import { Container, Typography, Tabs, Tab, Box } from "@mui/material";
+import { Tabs, Tab, Container, Box } from "@mui/material";
 import CreateUser from "../components/Users/CreateUser";
 import UserList from "../components/Users/UserList";
 import UserSearch from "../components/Users/UserSearch";
-// import Permissions from '../components/Users/Permissions';
+// import Permissions from "../components/Users/Permissions";
 
 const ManageUsersPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabChange = (event: React.ChangeEvent<{}>, newIndex: number) => {
-    setActiveTab(newIndex);
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setActiveTab(newValue);
   };
 
   return (
     <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Manage Users
-      </Typography>
-      <Tabs value={activeTab} onChange={handleTabChange}>
-        <Tab label="List Users" />
-        <Tab label="Search Users" />
-        <Tab label="Create User" />
-        <Tab label="Permissions" />
-      </Tabs>
-      <Box mt={3}>
-        {activeTab === 0 && <UserList />}
-        {activeTab === 1 && <UserSearch />}
+      <Box sx={{ width: "100%", bgcolor: "background.paper", mt: 2 }}>
+        <Tabs value={activeTab} onChange={handleTabChange} centered>
+          <Tab label="Search Users" />
+          <Tab label="List Users" />
+          <Tab label="Create User" />
+          <Tab label="Permissions" />
+        </Tabs>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        {activeTab === 0 && <UserSearch />}
+        {activeTab === 1 && <UserList />}
         {activeTab === 2 && <CreateUser />}
         {/* {activeTab === 3 && <Permissions />} */}
       </Box>
